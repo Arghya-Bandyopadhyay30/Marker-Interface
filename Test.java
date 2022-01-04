bc5dd718b41ff8bdc0cba580d1b249e7ec2a1734
Arghya Bandyopadhyay <64891347+Arghya-Bandyopadhyay30@users.noreply.github.com>
2022-01-04 10:13:16 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -4,12 +4,19 @@ class Test{
 		Car bmw =new Car("HF167X");
 
 		File f = new File("abc.txt");
-		FileOutputStream fout=new FileOutputStream("abc.txt");
+		FileOutputStream fout=new FileOutputStream(f);
 		ObjectOutputStream out=new ObjectOutputStream(fout);
 
 		out.writeObject(bmw);
 		out.flush();
 
 		System.out.println("Success");
+		
+		
+		FileInputStream fin = new FileInputStream(f);
+		ObjectInputStream in = new ObjectInputStream(fin);
+		Car ans = (Car) in.readObject();
+		
+		System.out.println("Car Number:" + ans.carPlate);
 	}
 }
